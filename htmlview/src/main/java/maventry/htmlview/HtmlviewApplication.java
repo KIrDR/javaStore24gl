2f8a68fc3d3318dffd45d85bb51e36ba15e4adcc
Kirill Drach <kirill.drach@sqlways.com>
2024-08-08 16:03:18 +0300

    dddd

---
+++ htmlview/src/main/java/maventry/htmlview/HtmlviewApplication.java
@@ -0,0 +1,13 @@
+package maventry.htmlview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlviewApplication.class, args);
+	}
+
+}
---
+++ htmlview/src/test/java/maventry/htmlview/HtmlviewApplicationTests.java
@@ -0,0 +1,13 @@
+package maventry.htmlview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
